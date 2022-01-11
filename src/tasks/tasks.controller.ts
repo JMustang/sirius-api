fcd2028f66102946b4da8d768520d7c22e38c1a5
junior <eecfredes@gmail.com>
2022-01-11 10:46:52 -0300

    feat:deleteTask created

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -21,5 +21,10 @@ export class TasksController {
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
   }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
+  }
 }
-// shift + alt + F = formata o codigo
+// vscode - shift + alt + F = formata o codigo no windows
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,14 +6,17 @@ import { CreateTaskDto } from './dto/create-task.dto';
 export class TasksService {
   private tasks: Task[] = [];
 
+  // Essa metodo devolve todas as tesks que existirem.
   getAllTasks(): Task[] {
     return this.tasks;
   }
 
+  // Essa metodo busca uma tesks pelo ID.
   getTaskById(id: string): Task {
     return this.tasks.find((task) => task.id === id);
   }
 
+  // Essa metodo cria uma tesks.
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
     const task: Task = {
@@ -26,4 +29,10 @@ export class TasksService {
     this.tasks.push(task);
     return task;
   }
+
+  // Essa metodo deleta uma tesks.
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 }
+// vscode - shift + alt + F = formata o codigo no windows
