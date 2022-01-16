43f37cf5bb341ae1490e65e006d612ebba62762e
junior <eecfredes@gmail.com>
2022-01-15 22:15:15 -0300

    feat:Validating Task Filtering and Search, add

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,7 +1,13 @@
 /* eslint-disable prettier/prettier */
+import { IsString, IsOptional, IsEnum } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
